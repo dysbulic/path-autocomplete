2797f2e29369ff7e88040060c82bdd94f779c3e7
Will Holcomb <will@dhappy.org>
2013-07-05 00:32:04 -0400

    adding academy awards

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ $( function() {
     var awards = {}
     loadAward( 'Hugo', 'book', '/en/hugo_award', awards )
     loadAward( 'Nebula', 'book', '/en/nebula_award', awards )
+    loadAward( 'Academy', 'movie', '/en/academy_awards', awards )
 
     $(".path.input").pathAutocomplete( awards )
 } )
